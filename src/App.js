9bd194a4212c59f3449bf3e7d8c4cf6e20383e27
Alex Vargas <yoralexvargas@gmai.com>
2020-06-26 15:42:33 -0400

    add state call postpage CommentSectionContainer

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ Look at each file to see where you need to pass props or add code
 */
 import React from "react";
 import "./App.css";
+import PostsPage from "../src/components/PostsContainer/PostsPage";
+import SearchBar from "../src/components/SearchBar/SearchBarContainer";
 // import the PostsPage and SearchBar and add them to the App
+
 
 
 const App = () => {
@@ -14,6 +17,8 @@ const App = () => {
   return (
     <div className="App">
       {/* Add imported components here to render them */}
+      <PostsPage />
+      <SearchBar />
     </div>
   );
 };
--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -5,11 +5,17 @@ import Comment from "./Comment";
 import "./Comment.css";
 
 const CommentSection = props => {
-  // Add state for the comments
+  // Add state for the comments(data)
+const [data] = useState(props);
 
   return (
     <div>
-      {/* map through the comments data and return the Comment component */}
+      {/* .map(derp) through the comments data and return the Comment component */}
+      {data.comments.map ( bob => (
+        <Comment comment={bob} key={bob.username}/>
+      
+      
+      ) ) }
       <CommentInput />
     </div>
   );
